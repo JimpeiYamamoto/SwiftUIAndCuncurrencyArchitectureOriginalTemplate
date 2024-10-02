1c648602259cf511a1abf9d9a08ab0146486c31d
JimpeiYamamoto <zinpei104@gmail.com>
2024-10-02 23:16:04 +0900

    Initial Commit

---
+++ ArchtectureTemplate/ArchtectureTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchtectureTemplateApp.swift
+//  ArchtectureTemplate
+//
+//  Created by 山本 迅平 on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ArchtectureTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
